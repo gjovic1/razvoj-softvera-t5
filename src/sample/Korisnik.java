1687e0e86ad0ab67991b568d29b0400fcc5dfb2f
gjovic1 <gjovic1@etf.unsa.ba>
2019-04-10 09:36:26 +0200

    Dodat Korisnik java bean

--- src/sample/Korisnik.java
+++ src/sample/Korisnik.java
@@ -1,5 +1,82 @@
 package sample;
 
-public class Korisnik {
+import javafx.beans.property.SimpleStringProperty;
 
+public class Korisnik {
+    private SimpleStringProperty ime = new SimpleStringProperty("");
+    private SimpleStringProperty prezime = new SimpleStringProperty("");
+    private SimpleStringProperty email = new SimpleStringProperty("");
+    private SimpleStringProperty username = new SimpleStringProperty("");
+    private SimpleStringProperty lozinka = new SimpleStringProperty("");
+
+    public Korisnik(){}
+
+    public Korisnik(String ime, String prezime, String email, String username, String lozinka)
+    {
+        this.ime=new SimpleStringProperty(ime);
+        this.prezime=new SimpleStringProperty(prezime);
+        this.email=new SimpleStringProperty(email);
+        this.username=new SimpleStringProperty(username);
+        this.lozinka=new SimpleStringProperty(lozinka);
+    }
+
+    public String getIme() {
+        return ime.get();
+    }
+
+    public SimpleStringProperty imeProperty() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime.set(ime);
+    }
+
+    public String getPrezime() {
+        return prezime.get();
+    }
+
+    public SimpleStringProperty prezimeProperty() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime.set(prezime);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public SimpleStringProperty emailProperty() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
+
+    public String getUsername() {
+        return username.get();
+    }
+
+    public SimpleStringProperty usernameProperty() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username.set(username);
+    }
+
+    public String getLozinka() {
+        return lozinka.get();
+    }
+
+    public SimpleStringProperty lozinkaProperty() {
+        return lozinka;
+    }
+
+    public void setLozinka(String lozinka) {
+        this.lozinka.set(lozinka);
+    }
 }
